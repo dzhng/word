702c5a1bbdbe3df9bfb0ab361e427edb67430fdd
Zachary Zimbler <zachzimbler@gmail.com>
2012-01-15 08:18:17 -0500

    more ui controls

--- js/menu.js
+++ js/menu.js
@@ -25,101 +25,203 @@ var Menu = function()
 
 	
 	//select cursor
-	this.mouseDownHandler = this.mouseDown.bind(this);
+	this.mouseDownHandler = this.cursorDown.bind(this);
 	var selectCursor= document.getElementById("pointerButton");
 	selectCursor.addEventListener("mousedown", this.mouseDownHandler, false);
 	
 	//select textBox
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.textBoxDown.bind(this);
 	var selectTextBox = document.getElementById("textBoxButton");
-	selectTextBox.addEventListener("mousedown", this.mouseDownHandler, false;
+	selectTextBox.addEventListener("mousedown", this.mouseDownHandler, false);
 			
 	//select insertImage	
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.insertImageDown.bind(this);
 	var selectImage = document.getElementById("insertImageButton");
 	selectImage.addEventListener("mousedown", this.mouseDownHandler, false);
 			
 	//select drawShape	
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.DrawShapesDown.bind(this);
 	var drawShape = document.getElementById("drawShapeButton");
 	drawShape.addEventListener("mousedown", this.mouseDownHandler, false);
 		
 	//select font	
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.selectFontDown.bind(this);
 	var selectFont = document.getElementById("font");
 	selectFont.addEventListener("mousedown", this.mouseDownHandler, false);
 
 	//select fontSize 	
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.fontSizeDown.bind(this);
 	var fontSize = document.getElementById("fontSize");
 	fontSize.addEventListener("mousedown", this.mouseDownHandler, false);
 
 
 	//select alignleft
-	this.mouseDownHandler = this.mouseDown.bind(this);
+	this.mouseDownHandler = this.alignLeftDown.bind(this);
 	var selectCursor= document.getElementById("textAlignLeft");
 	selectCursor.addEventListener("mousedown", this.mouseDownHandler, false);
 	
 	//select alignCenter
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.alignCenterDown.bind(this);
 	var alignCenter = document.getElementById("textAlignCenter");
-	alignCenter.addEventListener("mousedown", this.mouseDownHandler, false;
+	alignCenter.addEventListener("mousedown", this.mouseDownHandler, false);
 			
 	//select alignRight 
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.alignRightDown.bind(this);
 	var alignRight = document.getElementById("textAlignRight");
 	alignRight.addEventListener("mousedown", this.mouseDownHandler, false);
 			
 	//select justify	
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.justifyDown.bind(this);
 	var justifying= document.getElementById("textAlignJustify");
 	justifying.addEventListener("mousedown", this.mouseDownHandler, false);
 		
 	//select bold	
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.boldDown.bind(this);
 	var selectBold = document.getElementById("textBold");
 	selectBold.addEventListener("mousedown", this.mouseDownHandler, false);
 	
 	//select Italic
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.italicDown.bind(this);
 	var selectItalic = document.getElementById("textItalic");
 	selectItalic.addEventListener("mousedown", this.mouseDownHandler, false);
 
 	//select Underline	
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.underlineDown.bind(this);
 	var selectUnderline = document.getElementById("textUnderline");
 	selectUnderline.addEventListener("mousedown", this.mouseDownHandler, false);
 
 	//select Color 
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.colorDown.bind(this);
 	var selectColor = document.getElementById("textColor");
 	selectTextColor.addEventListener("mousedown", this.mouseDownHandler, false);
 
 	//select Line Spacing	
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.lineSpacingDown.bind(this);
 	var selectSpacing = document.getElementById("textLineSpacing");
 	selectBold.addEventListener("mousedown", this.mouseDownHandler, false);
 
 	//select subscritp	
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.subScriptDown.bind(this);
 	var selectSubScript = document.getElementById("textSubScript");
 	selectSubScript.addEventListener("mousedown", this.mouseDownHandler, false);
 
 	//select superscript	
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.suoerScriptDown.bind(this);
 	var selectSuperScript = document.getElementById("textSuperScript");
 	selectSuperScript.addEventListener("mousedown", this.mouseDownHandler, false);
 
 	//select strikethrough
-    this.mouseDownHandler = this.mouseDown.bind(this);
+    this.mouseDownHandler = this.strikethroughDown.bind(this);
 	var selectStrikethrough = document.getElementById("textStrikethrough");
 	selectStrikethrough.addEventListener("mousedown", this.mouseDownHandler, false);
 };
 
-Menu.prototype.mouseDown = function(e)
+
+Menu.prototype.cursorDown = function(e)
 {
 	this.pX = e.pageX;
 	this.pY = e.pageY;
-	console.log('something funny');
+};
+
+Menu.prototype.textBoxDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.insertImageDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.drawShapesDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.selectFontDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.fontSizeDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.alignLeftDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.alignCenterDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.alignRightDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.justifyDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.boldDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.italicDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.underlineDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.colorDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.lineSpacingDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.subScriptDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.superScriptDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
+};
+
+Menu.prototype.strikethroughDown = function(e)
+{
+	this.pX = e.pageX;
+	this.pY = e.pageY;
 };
 
