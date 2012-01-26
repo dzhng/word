77d428efcab24d8a3a05e3d17b6a9271e3fae769
david <dzz0615@gmail.com>
2012-01-25 21:34:58 -0800

    added three boxes template

--- js/templates.js
+++ js/templates.js
@@ -18,6 +18,29 @@ var TwoColumns = function(model)
 
 TwoColumns.prototype = new Page;
 
+var ThreeBoxes = function(model)
+{
+	var box0 = new TextBox(PPI, PPI,	// offset box by 1 inch from page by default;		
+					(PAGE_SIZE.width - 2*PPI)/2,
+					(PAGE_SIZE.height - 2*PPI)/3, this);
+
+	var box1 = new TextBox(PPI, 2*PPI+(PAGE_SIZE.height-2*PPI)/3,	// offset box by 1 inch from page by default;		
+					(PAGE_SIZE.width - 2*PPI)/2,
+					(PAGE_SIZE.height - 2*PPI)*2/3 - 2*PPI, this);
+
+	var box2 = new TextBox(PPI+(PAGE_SIZE.width-2*PPI)/2, PPI,	// offset box by 1 inch from page by default;		
+					(PAGE_SIZE.width - 2*PPI)/2,
+					(PAGE_SIZE.height - 2*PPI)/2, this);
+
+	box0.visible = true;
+	box1.visible = true;
+	box2.visible = true;
+	this.boxes = [box0, box1, box2];
+	this.model = model || null;
+};
+
+ThreeBoxes.prototype = new Page;
+
 var FourBoxes = function(model)
 {
 	var box0 = new TextBox(PPI, PPI,	// offset box by 1 inch from page by default;		
